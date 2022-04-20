ce1080f7d8e951878974afe4b22d2e2645b94265
raselinfo <raselinfo52@gmail.com>
2022-04-20 09:54:16 -0700

    Remove Duplication and update total_product propertytotal_product property

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 const colors = [
     { "name": "Black", "total_product": 1 },
+    { "name": "Black", "total_product": 1 },
     { "name": "White", "total_product": 1 },
     { "name": "White", "total_product": 1 },
     { "name": "White", "total_product": 1 },
-    { "name": "White", "total_product": 1 },
-    
-]
+    { "name": "White", "total_product": 1 }
+];
 
-let noneDuplicate = []
-let duplicate = []
-
-for (let i = 0; i < colors.length; i++) {
-    let item = colors[i]
-    let index = noneDuplicate.findIndex(innerItem => {
-        return item.name === innerItem.name
-    })
-
-    if (index === -1) {
-        noneDuplicate.push(item)
-    } else {
-        duplicate.push(item)
-    }
-}
-
-let result = noneDuplicate.map(item => {
-    let value = duplicate.find(findItem => findItem.name === item.name)
-    if (value) {
-        item.total_product += 2
-    }
-    return item
-})
-
-console.log(result)
-// console.log(duplicate)
+const counts = colors.reduce((acc, obj) => {
+    if (!acc[obj.name]) acc[obj.name] = { name: obj.name, total_product: 0 };
+    acc[obj.name].total_product++;
+    return acc;
+}, {});
+const result = Object.values(counts);
+console.log(result);
